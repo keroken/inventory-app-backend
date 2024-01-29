8e8cbc1caebd75549b04c781bbc94fc4dd9fb33e
keroken <kenichi.shinagawa@gmail.com>
2024-01-29 14:15:21 +0000

    add routing for purchases and sales

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('products/', views.ProductView.as_view())
+  path('products/', views.ProductView.as_view()),
+  path('purchases/', views.PurchaseView.as_view()),
+  path('sales/', views.SalesView.as_view()),
 ]
