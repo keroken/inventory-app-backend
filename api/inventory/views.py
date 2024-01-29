bfff3d6ce037df5121f2302e01866929120efbd5
keroken <kenichi.shinagawa@gmail.com>
2024-01-29 14:02:58 +0000

    add post method to Product view

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -8,15 +8,22 @@ class ProductView(APIView):
   """
   商品操作に関する関数
   """
-
   def get(self, request, format=None):
     """
     商品の一覧を取得する
     """
-
     queryset = Product.objects.all()
     serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data, status.HTTP_200_OK)
   
-
+  def post(self, request, format=None):
+    """
+    商品情報を登録する
+    """
+    serializer = ProductSerializer(data=request.data)
+    # validationを通らなかった場合、例外を投げる
+    serializer.is_valid(raise_exception=True)
+    # 検証したデータを永続化する
+    serializer.save()
+    return Response(serializer.data, status.HTTP_201_CREATED)
 
