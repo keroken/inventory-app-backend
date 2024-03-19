e7b655c743bd82a06e4ff6740077eb175084ed73
keroken <kenichi.shinagawa@gmail.com>
2024-03-19 16:08:25 +0000

    add token urls

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+  TokenObtainPairView,
+  TokenRefreshView,
+)
 
 urlpatterns = [
+  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('token/refresh/', TokenObtainPairView.as_view(), name='token_refresh'),
   path('products/', views.ProductView.as_view()),
   path('products/<int:id>/', views.ProductView.as_view()),
   path('purchases/', views.PurchaseView.as_view()),
