28221fd09e88779820a4a290efae63e5b143b469
keroken <kenichi.shinagawa@gmail.com>
2024-03-19 15:51:15 +0000

    add JWT Authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,3 +140,10 @@ LOGGING = {
         }
     }
 }
+
+REST_FRAMWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+  ),
+  'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.isAuthenticated']
+}
