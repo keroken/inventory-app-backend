32ef20bbe46265c23813c37ef02a87c641c1673b
keroken <kenichi.shinagawa@gmail.com>
2024-03-20 03:00:08 +0000

    update view class order

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -12,31 +12,6 @@ from rest_framework.exceptions import NotFound
 from .models import Product, Purchase, Sales
 from .serializers import InventorySerializer, ProductSerializer, PurchaseSerializer, SalesSerializer
 
-class LoginView(APIView):
-  """ユーザーのログイン処理
-
-  Args:
-    APIView (class): rest_framework.viewsのAPIViewを受け取る
-  """
-  # 認証クラスの指定
-  # リクエストヘッダーにtokenを差し込むカスタム動作をしないため、素の認証クラスを使用する
-  authentication_classes = [JWTAuthentication]
-  # アクセス許可の指定
-  permission_classes = []
-
-  def post(self, request):
-    serializer = TokenObtainPairSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    access = serializer.validated_data.get("access", None)
-    refresh = serializer.validated_data.get("refresh", None)
-    if access:
-      response = Response(status=status.HTTP_200_OK)
-      max_age = settings.COOKIE_TIME
-      response.set_cookie('access', access, httponly=True, max_age=max_age)
-      response.set_cookie('refresh', refresh, httponly=True, max_age=max_age)
-      return response
-    return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
-
 class ProductView(APIView):
   # 認証クラスの指定
   authentication_classes = [JWTAuthentication]
@@ -123,3 +98,28 @@ class InventoryView(APIView):
       serializer = InventorySerializer(queryset, many=True)
     return Response(serializer.data, status.HTTP_200_OK)
     
+class LoginView(APIView):
+  """ユーザーのログイン処理
+
+  Args:
+    APIView (class): rest_framework.viewsのAPIViewを受け取る
+  """
+  # 認証クラスの指定
+  # リクエストヘッダーにtokenを差し込むカスタム動作をしないため、素の認証クラスを使用する
+  authentication_classes = [JWTAuthentication]
+  # アクセス許可の指定
+  permission_classes = []
+
+  def post(self, request):
+    serializer = TokenObtainPairSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    access = serializer.validated_data.get("access", None)
+    refresh = serializer.validated_data.get("refresh", None)
+    if access:
+      response = Response(status=status.HTTP_200_OK)
+      max_age = settings.COOKIE_TIME
+      response.set_cookie('access', access, httponly=True, max_age=max_age)
+      response.set_cookie('refresh', refresh, httponly=True, max_age=max_age)
+      return response
+    return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
+  
