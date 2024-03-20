852eea79b2784dc259cd61e0d7348dbe894fc110
keroken <kenichi.shinagawa@gmail.com>
2024-03-20 01:09:28 +0000

    create login view class

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,4 +1,5 @@
 from api.inventory.exception import BusinessException
+from django.conf import settings
 from django.db.models import F, Value, Sum
 from django.db.models.functions import Coalesce
 from rest_framework import status
@@ -6,10 +7,35 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.exceptions import NotFound
 from .models import Product, Purchase, Sales
 from .serializers import InventorySerializer, ProductSerializer, PurchaseSerializer, SalesSerializer
 
+class LoginView(APIView):
+  """ユーザーのログイン処理
+
+  Args:
+    APIView (class): rest_framework.viewsのAPIViewを受け取る
+  """
+  # 認証クラスの指定
+  # リクエストヘッダーにtokenを差し込むカスタム動作をしないため、素の認証クラスを使用する
+  authentication_classes = [JWTAuthentication]
+  # アクセス許可の指定
+  permission_classes = []
+
+  def post(self, request):
+    serializer = TokenObtainPairSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    access = serializer.validated_data.get("access", None)
+    refresh = serializer.validate_data.get("refresh", None)
+    if access:
+      response = Response(status=status.HTTP_200_OK)
+      max_age = settings.COOKIE_TIME
+      response.set_cookie('access', access, httponly=True, max_age=max_age)
+      response.set_cookie('refresh', refresh, httponly=True, max_age=max_age)
+      return response
+    return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class ProductView(APIView):
   # 認証クラスの指定
